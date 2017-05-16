493edcb0518c6509e1f8e29f7aad7535f0101d47
alfonso zbook <zbook@reyesaguayo.com>
2017-05-16 11:17:34 -0500

    fix Pendulum with Euler solver

--- R/PendulumEuler.R
+++ R/PendulumEuler.R
@@ -2,11 +2,11 @@
 # Pendulum.R
 #
 source("./R/ODE.R")
-source("./R/EulerRichardson.R")
+source("./R/Euler.R")
 
 
 
-setClass("Pendulum", slots = c(
+setClass("PendulumEuler", slots = c(
     omega0Squared = "numeric",
     state = "numeric",
     odeSolver = "Euler"
@@ -18,25 +18,32 @@ setClass("Pendulum", slots = c(
     contains = c("ODE")
     )
 
-setMethod("initialize", "Pendulum", function(.Object) {
+setMethod("initialize", "PendulumEuler", function(.Object) {
     .Object@odeSolver <- Euler(.Object)                               
     return(.Object)
 })
 
-setMethod("setStepSize", "Pendulum", function(object, stepSize, ...) {
-    object@odeSolver <- setStepSize(object@odeSolver, stepSize)
+# setMethod("setStepSize", "PendulumEuler", function(object, stepSize, ...) {
+#     object@odeSolver <- setStepSize(object@odeSolver, stepSize)
+#     object
+# })
+
+setMethod("setStepSize", signature("PendulumEuler"), function(object, dt, ...) {
+    # use explicit parameter declaration
+    # setStepSize generic may use two different step parameters: stepSize and dt
+    object@odeSolver <- setStepSize(object@odeSolver, dt)
     object
 })
 
 
-setMethod("step", "Pendulum", function(object) {
+setMethod("step", "PendulumEuler", function(object) {
     object@odeSolver <- step(object@odeSolver)
     object@rate  <- object@odeSolver@ode@rate                           
     object@state <- object@odeSolver@ode@state                          
     object
 })
 
-setMethod("setState", "Pendulum", function(object, theta, thetaDot) {
+setMethod("setState", "PendulumEuler", function(object, theta, thetaDot) {
     object@state[1] <- theta     # angle
     object@state[2] <- thetaDot  # derivative of angle
     #                              state[3] is time
@@ -44,21 +51,21 @@ setMethod("setState", "Pendulum", function(object, theta, thetaDot) {
     object
 })
 
-setMethod("getState", "Pendulum", function(object) {                
+setMethod("getState", "PendulumEuler", function(object) {                
     object@state
 })
 
 
-setMethod("getRate", "Pendulum", function(object, state, rate) {    
+setMethod("getRate", "PendulumEuler", function(object, state, rate) {    
     rate[1] <- state[2]     # rate of change of angle                                      # diff 11
     rate[2] <- -object@omega0Squared * sin(state[1])  # rate of change of dtheta                 
     rate[3] <- 1            # rate of change of time, dt/dt
     
     object@state <- object@odeSolver@ode@state <- state
     object@rate  <- object@odeSolver@ode@rate  <- rate
-    object@rate                                                       #
+    object                                                   #
 })
 
 
 # constructor
-Pendulum <- function()  new("Pendulum")
+PendulumEuler <- function()  new("PendulumEuler")
