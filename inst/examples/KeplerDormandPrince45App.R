c15c6f06617b24eb90cb6cdab359952039efc636
alfonso zbook <zbook@reyesaguayo.com>
2017-05-17 18:23:53 -0500

    make app for testing Kepler with DormandPrince45

--- inst/examples/KeplerDormandPrince45App.R
+++ inst/examples/KeplerDormandPrince45App.R
@@ -1,6 +1,7 @@
-#' KeplerVerletApp.R
+#' KeplerDormandPrince45App.R
 #' 
 #' Demostration of the use of ODE solver RK45
+#' Brought from original routine for testing Verlet ODE solver
 #' 
 #' 
 source("./R/ode_generics.R")
@@ -27,18 +28,20 @@ odeSolver <- setTolerance(odeSolver, tol)   # this works for adaptive solvers
 
 particle@odeSolver <- odeSolver
 
-# odeSolver <- setTolerance(odeSolver, tol)
-# particle@odeSolver <- init(particle@odeSolver, dt)
-
 
 
 initialEnergy <- getEnergy(particle)
 i <- 0
-while (i < 100) {
+while (getTime(particle) < 1.5) {
     # odeSolver <- step(odeSolver)
     particle <- doStep(particle)
     # odeSolver <- particle@odeSolver
     energy <- getEnergy(particle)
-    cat(sprintf("time=%12f energy=%12f state[5]=%12f \n", getTime(particle), energy, particle@state[5]))
+    cat(sprintf("time=%12f energy=%12f state[5]=%12f x=%10f y=%10f \n", getTime(particle), 
+                energy, particle@state[5], particle@state[1], particle@state[3]))
     i <- i + 1
 }
+# Values from Java at t approx = 1.5 
+# time=    1.444831 energy=  -19.737930 state[4]=    1.444831 
+# time=    1.507215 energy=  -19.737875 state[4]=    1.507215 
+# time=    1.569599 energy=  -19.737819 state[4]=    1.569599
