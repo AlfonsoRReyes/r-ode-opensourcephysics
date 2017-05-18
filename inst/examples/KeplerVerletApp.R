ce39fa9265b7561ed26433d3a70ead237ab4f4a2
alfonso zbook <zbook@reyesaguayo.com>
2017-05-18 10:15:31 -0500

    add output from Java and R to the bottom of file

--- inst/examples/KeplerVerletApp.R
+++ inst/examples/KeplerVerletApp.R
@@ -32,7 +32,7 @@ particle@odeSolver <- odeSolver
 
 initialEnergy <- getEnergy(particle)
 i <- 0
-while (getTime(particle) <= 0.33) {
+while (getTime(particle) <= 1.20) {
     # odeSolver <- step(odeSolver)
     particle <- doStep(particle)
     # odeSolver <- particle@odeSolver
@@ -40,3 +40,25 @@ while (getTime(particle) <= 0.33) {
     cat(sprintf("time=%12f energy=%12f state[5]=%12f \n", getTime(particle), energy, particle@state[5]))
     i <- i + 1
 }
+
+#' output from Java run
+#' 
+# time=    0.970000 energy=  -19.739208 state[4]=    0.970000 
+# time=    0.980000 energy=  -19.739208 state[4]=    0.980000 
+# time=    0.990000 energy=  -19.739209 state[4]=    0.990000 
+# time=    1.000000 energy=  -19.739209 state[4]=    1.000000 
+# time=    1.010000 energy=  -19.739209 state[4]=    1.010000 
+# time=    1.020000 energy=  -19.739209 state[4]=    1.020000 
+# time=    1.030000 energy=  -19.739208 state[4]=    1.030000 
+# time=    1.040000 energy=  -19.739208 state[4]=    1.040000 
+
+#' output from R run
+#' 
+# time=    0.970000 energy=  -19.739208 state[5]=    0.970000 
+# time=    0.980000 energy=  -19.739208 state[5]=    0.980000 
+# time=    0.990000 energy=  -19.739209 state[5]=    0.990000 
+# time=    1.000000 energy=  -19.739209 state[5]=    1.000000 
+# time=    1.010000 energy=  -19.739209 state[5]=    1.010000 
+# time=    1.020000 energy=  -19.739209 state[5]=    1.020000 
+# time=    1.030000 energy=  -19.739208 state[5]=    1.030000 
+# time=    1.040000 energy=  -19.739208 state[5]=    1.040000 
